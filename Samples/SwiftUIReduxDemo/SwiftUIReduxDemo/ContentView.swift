145f7203c370c718d35ccc8fb7043ed5b69e6514
Cheng Zhang <showt2me@gmail.com>
2020-04-06 20:47:30 +0900

    FeedListView

--- Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/ContentView.swift
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/ContentView.swift
@@ -1,21 +1,13 @@
-//
-//  ContentView.swift
-//  SwiftUIReduxDemo
-//
-//  Created by Cheng Zhang on 4/6/20.
-//  Copyright © 2020 Cheng Zhang. All rights reserved.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct FeedListView: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FeedListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FeedListView()
     }
 }
