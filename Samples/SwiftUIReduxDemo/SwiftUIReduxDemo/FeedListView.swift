ca3b6d83316ed4617dace25c183b097f50c8d1db
Cheng Zhang <showt2me@gmail.com>
2020-04-06 20:51:49 +0900

    FeedListView

---
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/FeedListView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+public class Feed: Identifiable {
+  public let id = UUID()
+  public let title: String
+  public var isLiked: Bool
+  
+  public init(title: String, isLiked: Bool = false) {
+    self.title = title
+    self.isLiked = isLiked
+  }
+}
+
+struct FeedCell: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct FeedListView: View {
+  var body: some View {
+    
+    List {
+      ForEach(0..<10) { id in
+        FeedCell()
+      }
+    }
+  }
+}
+
+struct FeedListView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeedListView()
+  }
+}
