013bb1b2cb4df8c99304e332e46bbe056b3c53be
Cheng Zhang <showt2me@gmail.com>
2020-04-06 19:30:37 +0900

    Init project

---
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIReduxDemo
+//
+//  Created by Cheng Zhang on 4/6/20.
+//  Copyright © 2020 Cheng Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIRedux/SwiftUIRedux/SwiftUIRedux.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIRedux.h
+//  SwiftUIRedux
+//
+//  Created by Cheng Zhang on 4/6/20.
+//  Copyright © 2020 Cheng Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIRedux.
+FOUNDATION_EXPORT double SwiftUIReduxVersionNumber;
+
+//! Project version string for SwiftUIRedux.
+FOUNDATION_EXPORT const unsigned char SwiftUIReduxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIRedux/PublicHeader.h>
+
+
