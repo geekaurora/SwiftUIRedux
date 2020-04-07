f3439f703c7ef8196247edf26b24ed4c4fcb5254
Cheng Zhang <showt2me@gmail.com>
2020-04-07 19:09:46 +0900

    minor

--- Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/NotificationList/NotificationListState.swift
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/NotificationList/NotificationListState.swift
@@ -6,20 +6,20 @@ class NotificationListState: Subscriber, ObservableObject {
   
   @Published var notifications: [Notification] = Notification.mocks
   
-//  public override func reduce(action: ReduxActionProtocol) {
-//    switch action {
-//    case let action as FeedLikeAction:
-//      // Update corresponding feed `isLiked`, and then reload UI by set `self.feeds`.
-//      self.notifications = notifications.map { notification in
-//        if notification.feed.Id == action.feed.Id {
-//          notification.id = UUID()
-//          notification.feed.isLiked = !notification.feed.isLiked
-//        }
-//        return notification
-//      }
-//    default:
-//      break
-//    }
-//  }
+  public override func reduce(action: ReduxActionProtocol) {
+    switch action {
+    case let action as FeedLikeAction:
+      // Update corresponding feed `isLiked`, and then reload UI by set `self.feeds`.
+      self.notifications = notifications.map { notification in
+        if notification.feed.Id == action.feed.Id {
+          notification.id = UUID()
+          notification.feed.isLiked = !notification.feed.isLiked
+        }
+        return notification
+      }
+    default:
+      break
+    }
+  }
   
 }
