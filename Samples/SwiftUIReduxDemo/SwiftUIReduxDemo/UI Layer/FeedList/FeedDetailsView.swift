e49aade14c0f9bfaeffdd74cdb4c3c4a328f16dc
Cheng Zhang <showt2me@gmail.com>
2020-04-07 21:39:08 +0900

    minor

--- Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/FeedList/FeedDetailsView.swift
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/FeedList/FeedDetailsView.swift
@@ -18,6 +18,10 @@ class FeedDetailsState: Subscriber, ObservableObject {
 struct FeedDetailsView: View {
   @ObservedObject var state: FeedDetailsState
   
+  init(feed: Feed) {
+    self.state = FeedDetailsState(feed: feed)
+  }
+  
   var body: some View {
     HStack {
       Text(state.feed.title)
