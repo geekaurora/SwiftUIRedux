b9bf4ed087b5cd4289adae9f63ec3c86a425652d
Cheng Zhang <showt2me@gmail.com>
2020-04-07 23:38:09 +0900

    recommend struct

--- SwiftUIRedux/SwiftUIRedux/State.swift
+++ SwiftUIRedux/SwiftUIRedux/State.swift
@@ -3,6 +3,14 @@ import SwiftUIKit
 /// Protocol of Redux State.
 public protocol ReduxStateProtocol {
   /// Reacts to the `action`. Note that subscribers can filter action and choose whether to respond.
+  ///
+  /// - Note:
+  /// Recommendation is to define conformance as struct and return deep copy of self,  e.g. var newSelf = self,
+  /// to avoid duplicate mutations on the same model if with reference type.
+  ///
+  /// For example, if FeedList/FeedDetails shares the same `Feed` refence, LikeFeed action
+  /// in FeedDetails will possibly mutate `Feed` instance twice by both `reduce(action:)`
+  /// in FeedDetailsState and FeedListState.
   @discardableResult
   func reduce(action: ReduxActionProtocol) -> Self
 }
