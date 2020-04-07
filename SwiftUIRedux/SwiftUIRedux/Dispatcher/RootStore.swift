1d768da8edf821ce425259897c74905bb54d8e53
Cheng Zhang <showt2me@gmail.com>
2020-04-07 19:06:46 +0900

    minor

--- SwiftUIRedux/SwiftUIRedux/Dispatcher/RootStore.swift
+++ SwiftUIRedux/SwiftUIRedux/Dispatcher/RootStore.swift
@@ -1,8 +1,6 @@
 import CZUtils
 import SwiftUIKit
 
-public typealias SubscriberObject = NSObject & SubscriberProtocol
-
 /// Convenience method to dispatch `action` to subscribers of `RootStore.shared`.
 public func dispatch(action: ReduxActionProtocol) {
   RootStore.shared.dispatch(action: action)
@@ -21,20 +19,20 @@ public class RootStore {
   public func dispatch(action: ReduxActionProtocol = ReduxReloadAction()) {
     // Notify all subscribers. Note that subscribers can filter action and choose whether to respond.
     subscribers.allObjects
-      .compactMap { ($0 as? SubscriberObject).assertIfNil }
+      .compactMap { ($0 as? Subscriber).assertIfNil }
       .forEach { $0.reduce(action: action) }
   }
 
-  public func addSubscriber(_ subscriber: SubscriberObject) {
+  public func addSubscriber(_ subscriber: Subscriber) {
     guard !containsSubscriber(subscriber) else { return }
     subscribers.add(subscriber)
   }
 
-  public func removeSubscriber(_ subscriber: SubscriberObject) {
+  public func removeSubscriber(_ subscriber: Subscriber) {
     subscribers.remove(subscriber)
   }
 
-  public func containsSubscriber(_ subscriber: SubscriberObject) -> Bool {
+  public func containsSubscriber(_ subscriber: Subscriber) -> Bool {
     return subscribers.contains(subscriber)
   }
 
