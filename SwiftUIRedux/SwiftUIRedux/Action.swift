9d5d8cda4d1a709ec414b1007a55126a28c331b6
Cheng Zhang <showt2me@gmail.com>
2020-04-09 16:55:24 +0900

    minor

--- SwiftUIRedux/SwiftUIRedux/Action.swift
+++ SwiftUIRedux/SwiftUIRedux/Action.swift
@@ -3,9 +3,6 @@ import SwiftUIKit
 /// Protocol that defines Redux Action - different from User Action.
 public protocol ReduxActionProtocol {}
 
-/// Protocol that defines Redux Command - asynchronous Action.
-public protocol ReduxCommandProtocol {}
-
 /// Redux Action that triggers UI reload.
 public class ReduxReloadAction: ReduxActionProtocol {
   public init() {}
---
+++ SwiftUIRedux/SwiftUIRedux/Command.swift
@@ -0,0 +1,4 @@
+import SwiftUIKit
+
+/// Protocol that defines Redux Command - asynchronous Action.
+public protocol ReduxCommandProtocol {}
