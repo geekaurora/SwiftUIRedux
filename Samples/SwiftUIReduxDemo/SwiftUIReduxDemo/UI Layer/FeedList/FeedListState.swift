605d84988a74de542b1ce8a3f29066b0376398df
Cheng Zhang <showt2me@gmail.com>
2020-04-09 17:07:28 +0900

    fetch feeds

--- Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/FeedList/FeedListState.swift
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/UI Layer/FeedList/FeedListState.swift
@@ -4,7 +4,17 @@ import CZUtils
 
 public class FeedListState: Subscriber, ObservableObject {
   
-  @Published var feeds: [Feed] = Feed.mocks
+  //@Published var feeds: [Feed] = Feed.mocks
+  @Published var feeds: [Feed] = []
+  
+  public override init() {
+    super.init()
+    
+    // Asynchronous Command - fetch feeds.
+    Services.shared.fetchFeeds { feeds in
+      self.feeds = feeds
+    }
+  }
   
   public override func reduce(action: ReduxActionProtocol) {
     // No need of deep copy, SwiftUI decides wether to reload by List diff.
