1ebef95c9cb4f0f61c9792798561e837ddf3bd00
Cheng Zhang <showt2me@gmail.com>
2020-04-09 18:08:03 +0900

    minor

--- Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/Net Layer/Services.swift
+++ Samples/SwiftUIReduxDemo/SwiftUIReduxDemo/Net Layer/Services.swift
@@ -7,10 +7,10 @@ class Services {
   func fetchFeeds(endPoint: String,
                   params: [String: AnyObject] = [:],
                   completion: @escaping ([Feed]) -> Void) {
-    // Mock asynchronous callback of fetching feeds.
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
       let feeds = Feed.mocks
       completion(feeds)
     }
   }
+  
 }
