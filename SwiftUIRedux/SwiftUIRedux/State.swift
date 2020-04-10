f32927ecf0cf4896f39cfbf5fbdf661c48d6b77a
Cheng Zhang <showt2me@gmail.com>
2020-04-10 19:20:41 +0900

    minor

--- SwiftUIRedux/SwiftUIRedux/State.swift
+++ SwiftUIRedux/SwiftUIRedux/State.swift
@@ -5,18 +5,16 @@ public protocol ReduxStateProtocol {
   /// Reduce function that generates new State of self with the input `action`.
   /// i.e. (oldState, action) => newState
   ///
-  /// P.S. State can update parts of deep copy of itself or filter `action` and choose whether to respond.
-  ///
   /// - Note:
-  /// Recommendation is to define conformance as struct and return deep copy of self,  e.g. var newSelf = self,
-  /// to avoid duplicate mutations on the same instance if `self` is reference type.
+  /// Recommendation is to define conformance as `struct` and return deep copy of self,  e.g. var newSelf = self,
+  /// so to avoid duplicate mutations on the same instance if `self` is reference type.
   /// If you have to use reference type, workaround is to return deep copy of `self`.
   ///
   /// For example, if FeedList/FeedDetails shares the same `Feed` class reference, LikeFeed action
-  /// in FeedDetails will possibly mutate `Feed` instance twice by `reduce(action:)` function
-  /// in both FeedDetailsState and FeedListState.
+  /// in FeedDetails will possibly mutate `Feed` instance twice trigged by `reduce(action:)` function
+  /// in both FeedList/FeedDetails.
   ///
-  /// - Parameter action: The action that dispatchs to the current state.
+  /// - Parameter action: The action that be used to reduce.
   /// - Returns: The new State of self after reducing `action`.
   @discardableResult
   func reduce(action: ReduxActionProtocol) -> Self
