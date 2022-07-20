070e890771e456f5716d57828207911a90c7a1c0
Cheng Zhang <showt2me@gmail.com>
2022-07-19 18:20:46 -0700

    [Dependency] Remove the dependency on SwiftUIKit.

--- Package.swift
+++ Package.swift
@@ -16,14 +16,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.4.6"),
-        .package(url: "https://github.com/geekaurora/SwiftUIKit.git", from: "1.0.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUIRedux",
-            dependencies: ["CZUtils", "SwiftUIKit"]),
+            dependencies: ["CZUtils"]),
         .testTarget(
             name: "SwiftUIReduxTests",
             dependencies: ["SwiftUIRedux"]),
