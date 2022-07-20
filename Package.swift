872716a8460d0a0f4dad16822aad6cb8d14f0ee6
Cheng Zhang <showt2me@gmail.com>
2022-07-19 18:28:05 -0700

    [Dependency] Bump CZUtils to 4.4.0.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.4.6"),
+        .package(url: "https://github.com/geekaurora/CZUtils.git", from: "4.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
