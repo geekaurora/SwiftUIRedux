b157970ebba27098c543966f6b470a74ed4e2c99
cheng zhang <showt2me@gmail.com>
2024-06-02 11:58:54 -0700

    [Required iOS] Update required iOS version to 15.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIRedux",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS("15.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
